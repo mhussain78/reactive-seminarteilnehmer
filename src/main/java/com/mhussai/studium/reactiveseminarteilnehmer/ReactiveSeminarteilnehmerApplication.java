bdd37ea9e66e2b4fb755430baab5201ce6e93053
Mohamad Hussain <mohamad.hussain@live.de>
2019-12-28 16:25:54 +0100

    project initialized

---
+++ src/main/java/com/mhussai/studium/reactiveseminarteilnehmer/ReactiveSeminarteilnehmerApplication.java
@@ -0,0 +1,13 @@
+package com.mhussai.studium.reactiveseminarteilnehmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSeminarteilnehmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSeminarteilnehmerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhussai/studium/reactiveseminarteilnehmer/ReactiveSeminarteilnehmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhussai.studium.reactiveseminarteilnehmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSeminarteilnehmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
